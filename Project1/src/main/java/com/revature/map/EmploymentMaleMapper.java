39f470953a5b29cbfaa57f3c65dbe18f4ab1a770
vince4188 <cloudera@quickstart.cloudera>
2018-12-13 21:13:30 -0800

    documentation

--- Project1/src/main/java/com/revature/map/EmploymentMaleMapper.java
+++ Project1/src/main/java/com/revature/map/EmploymentMaleMapper.java
@@ -17,7 +17,9 @@ public class EmploymentMaleMapper extends Mapper<LongWritable, Text, Text, Doubl
 		line = line.substring(1).substring(0, line.length()-3);
 		String[] splits = line.split("\",\"");
 		
+		//used to find the only two lines I'm using
 		if (line.toLowerCase().contains("unemployment") && line.toLowerCase().contains(" male ") && line.toLowerCase().contains("ilo")){
+			//used to change format of message
 			String newKey = "(" + splits[0] + ") Percent change in unemployment,";
 			if(line.contains("youth")){
 				newKey += " youth male(% male labor force age 15-24)";
@@ -30,6 +32,7 @@ public class EmploymentMaleMapper extends Mapper<LongWritable, Text, Text, Doubl
 			if (splits.length > 4){
 				int count = 0;
 				double prevVal = 0.0;
+				//just like before, used to find difference starting from 2000-2016
 				for (int i = 44; i < splits.length; i++){
 					if(!splits[i].isEmpty() && !splits[i].trim().trim().equals("\"")) {
 						count++;
@@ -41,6 +44,7 @@ public class EmploymentMaleMapper extends Mapper<LongWritable, Text, Text, Doubl
 				}
 				//if count is 0 means there was no data after year 2000
 				if (count == 0){
+					//the -1.0 will be used to stop the reducer from processing the data should it recieve one
 					context.write(new Text(newKey), new DoubleWritable(-1.0));
 				}
 			}
